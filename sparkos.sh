760802e0655dcb38d9921f639bce8c3a17448102
Khalil.B <80763333+katakishi0@users.noreply.github.com>
2024-08-06 09:36:39 -0300

    Update sparkos.sh

--- sparkos.sh
+++ sparkos.sh
@@ -1,14 +1,11 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Spark-Rom/manifest -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-projects/local_manifest --depth 1 -b spark .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/crdroidandroid/android.git -b 11.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/katakishi0/local_manifest --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch spark_rosy-userdebug
-export TZ=Asia/Jakarta
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=userngebug
+lunch lineage_channel-userdebug
+export TZ=Asia/Dhaka #put before last build command
 mka bacon
-
 # end
